501b7f720815bbf192eb925bb7b69281b297a42c
NanthawatKTY <nanthawat.tae123@gmail.com>
2022-10-21 11:31:58 +0700

    Added folder directories

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,33 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    // we can have any runtime config here that is available in the server and client
+    // publicRuntimeConfig: {
+    //     WEATHER_APP_SECRET_KEY: process.env.WEATHER_APP_SECRET_KEY,
+    // },
 
+    // // we can have any runtime config here that is only available in the server
+    // privateRuntimeConfig: {
+    //     PRIVATE_MSG: 'This is a private message in the server not available in the client',
+    // },
+    css: ["assets/css/tailwind.css"],
+    modules: [
+        '@nuxtjs/tailwindcss',
+        // '@pinia/nuxt',
+    ],
+    meta: {
+        title: 'TODO App with Nuxt3',
+    },
+    // server: {
+    //     port:3333,
+    // },
+    build: {
+        postcss: {
+            postcssOptions: {
+                plugins: {
+                    tailwindcss: {},
+                    autoprefixer: {},
+                },
+            },
+        }
+    },
 })
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="flex justify-center items-center h-screen">
+      <h1 class="text-3xl font-bold text-green-500">
+        TODO APP With REST-API.
+      </h1>
+    </div>
+</template>
